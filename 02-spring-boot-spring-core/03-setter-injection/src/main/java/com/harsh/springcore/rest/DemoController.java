25a15c3d00d7c434825ab3d5eba09bb343d4fa92
HarshvardhanPatil4700 <harshpatil4700@gmail.com>
2026-08-05 17:43:23 +0530

    First commit

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/harsh/springcore/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.harsh.springcore.rest;
+
+import com.harsh.springcore.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Marks this class as a Spring Controller. It handles HTTP requests from the browser or client.
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
